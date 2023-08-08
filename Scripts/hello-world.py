f86f1feb7a69d19a87e1c69e4cc018535034f06b
BuildTools <unconfigured@null.spigotmc.org>
2023-08-08 17:58:42 -0500

    messing around in python

--- Scripts/hello-world.py
+++ Scripts/hello-world.py
@@ -3,3 +3,13 @@ print("hello world from python!")
 print("hello world from python!")
 print("hello world from python!")
 print("hello world from python!")
+
+name = 'Zane'
+print(name)
+name += ' Klausing'
+print(name)
+firstArray = [1, 2, 3, 4]
+print(firstArray)
+
+for x in firstArray:
+    print(x)
